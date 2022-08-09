e67690d30393c2b69e4b24833ed99931a7ad73c2
Seunghwan <ysp1307@naver.com>
2022-08-09 22:06:44 +0900

    feat: emotion set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Router from "./router/Router";
-
+import { Global } from "@emotion/react";
+import ResetStyle from "./styles/GlobalStyle";
 function App() {
   return (
     <div className="App">
+      <Global styles={ResetStyle} />
       <Router />
     </div>
   );
