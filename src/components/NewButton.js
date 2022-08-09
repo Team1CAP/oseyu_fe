697ea507faeca1bc05d8f8206dc2cbb7800d598b
Seunghwan <ysp1307@naver.com>
2022-08-09 22:06:58 +0900

    feat: emotion test

--- src/components/NewButton.js
+++ src/components/NewButton.js
@@ -1,8 +1,19 @@
+/** @jsxImportSource @emotion/react */
+
+import { css } from "@emotion/react";
+
+const btn = css`
+  width: 100px;
+  padding: 10px;
+  cursor: pointer;
+`;
+
 const NewButton = ({ text, type, onClick }) => {
   const btnType = ["positive", "negative"].includes(type) ? type : "default";
 
   return (
     <button
+      css={btn}
       className={["NewButton", `NewButton_${btnType}`].join(" ")}
       onClick={onClick}
     >
