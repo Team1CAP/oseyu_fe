b5461ad273cf30115cc64437761d347624c87ac3
Seunghwan <ysp1307@naver.com>
2022-08-31 16:24:08 +0900

    feat: LandingPage 추가

---
+++ src/pages/LandingPage/Landing.js
@@ -0,0 +1,23 @@
+import NewButton from "../../components/NewButton";
+import { useNavigate } from "react-router-dom";
+
+const Landing = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col h-[100vh] items-center justify-around bg-main-yellow ">
+      <div className="text-5xl font-bold  text-main-dark-green">OSEYU</div>
+      <label className="cursor-pointer bg-main-dark-green hover:bg-main-mid-green text-white font-bold py-3 px-10 w-60 rounded">
+        <NewButton
+          text={"시작하기"}
+          onClick={() => {
+            navigate("/main");
+          }}
+          type={"positive"}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default Landing;
