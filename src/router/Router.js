d0fc924f4a1959a04d127505f021f7cb67959d09
Seunghwan <ysp1307@naver.com>
2022-08-31 16:24:29 +0900

    feat: LandingPage 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,15 +3,19 @@ import Main from "../pages/MainPage/Main";
 import Login from "../pages/LoginPage/Login";
 import Edit from "../pages/EditPage/Edit";
 import Detail from "../pages/DetailPage/Detail";
+import Landing from "../pages/LandingPage/Landing";
+import Admin from "../pages/AdminPage/Admin";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/main" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/edit" element={<Edit />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
