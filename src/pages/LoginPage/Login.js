cfc677d5aa067e235e6317a563e948b80e5e960e
Seunghwan <ysp1307@naver.com>
2022-08-31 16:28:17 +0900

    feat: LoginPage 1차 퍼블리싱 완료

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,8 +1,71 @@
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <div>로그인페이지</div>
-    </>
+    <div className="flex flex-col items-center">
+      <div className="text-5xl font-bold  text-main-dark-green mt-32">
+        OSEYU
+      </div>
+      <form className="w-full max-w-sm mt-28">
+        <div className="mb-6">
+          <div className="">
+            <label
+              className="block text-gray-500 font-bold text-left mb-1 pr-4"
+              for="inline-full-name"
+            >
+              ID
+            </label>
+          </div>
+          <div>
+            <input
+              className="shadow appearance-none border-2 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-main-mid-green"
+              id="inline-full-name"
+              type="text"
+              placeholder="Seunghwan Kim"
+            />
+          </div>
+        </div>
+        <div className="mb-6">
+          <div>
+            <label
+              className="block text-gray-500 font-bold text-left mb-1 pr-4"
+              for="inline-password"
+            >
+              Password
+            </label>
+          </div>
+          <div>
+            <input
+              className="shadow  appearance-none border-2  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white  focus:border-main-mid-green"
+              id="inline-password"
+              type="password"
+              placeholder="********"
+            />
+          </div>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-500 font-bold text-left">
+            <span className="font-bold text-sm text-blue-500 hover:text-blue-800">
+              비밀번호를 잊어버리셨나요?
+            </span>
+          </label>
+        </div>
+        <div>
+          <div>
+            <button
+              className="shadow bg-main-dark-green hover:bg-main-mid-green focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="button"
+              onClick={() => {
+                navigate("/main");
+              }}
+            >
+              시작하기
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
