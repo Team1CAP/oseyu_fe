079fce9a076740cb196a19d98a2e9cb625b19538
Seunghwan <ysp1307@naver.com>
2022-08-31 22:56:44 +0900

    feat: tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      screen: "500px",
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "main-yellow": "#F7D281",
+        "main-light-yellow": "#FDF6E4",
+        "main-dark-green": "#2D4633",
+        "main-mid-green": "#77AB56",
+        "main-light-green": "#B6E399",
+      },
+    },
   },
   plugins: [],
 };
