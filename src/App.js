976deb2c4676b994a1b114d8d982964f224e2fe2
Seunghwan <ysp1307@naver.com>
2022-10-07 12:30:45 +0900

    ✨ feat : recoil 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Router from "./router/Router";
+import { RecoilRoot } from "recoil";
 function App() {
   return (
-    <div className="container text-center min-h-screen justify-center font-san">
-      <Router />
-    </div>
+    <RecoilRoot>
+      <div className="container text-center min-h-screen justify-center font-san">
+        <Router />
+      </div>
+    </RecoilRoot>
   );
 }
 
