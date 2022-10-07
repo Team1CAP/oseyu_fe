58c338e67844077b94b1c0d4e328fa1dadf140a1
Seunghwan <ysp1307@naver.com>
2022-10-07 12:33:14 +0900

    ✨ feat : 토큰 받기 준비 작업

--- src/pages/LoginPage/Callback.js
+++ src/pages/LoginPage/Callback.js
@@ -1,16 +1,47 @@
 import axios from "axios";
+import React, { useEffect } from "react";
+//import { useNavigate } from "react-router-dom";
+import { atom, useRecoilState } from "recoil";
+
+export const userDataState = atom({
+  key: "userDataState",
+  default: {
+    isLogin: false,
+  },
+});
+
 const Callback = () => {
-  let params = new URL(document.URL).searchParams;
-  let code = params.get("code");
-  //cors오류찾기
-  axios
-    .get(`http://43.200.157.167:3000/api/auth/kakao/login?code=${code}`)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  //let navigate = useNavigate();
+  let code = new URL(document.URL).searchParams.get("code");
+  const [userData, setUserData] = useRecoilState(userDataState); // 추후에 쪼개기 refact
+
+  useEffect(() => {
+    axios
+      .get(`/api/auth/kakao/login?code=${code}`)
+      .then((res) => {
+        console.log(res);
+        // 대충 이런느낌?
+        // sessionStorage.setItem('Token', response.data.token);
+        // localStorage.setItem('username', response.data.username);
+        setUserData({
+          ...userData,
+          code: res.data.code,
+          isLogin: true,
+          // username: response.data.username,
+          // nickname: response.data.nickname,
+          // profileImgUrl: response.data.profileImgUrl
+
+          // ~~~~
+        });
+
+        //navigate("/main");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(userData);
 
   return (
     <div className="flex flex-col h-screen items-center justify-around bg-main-light-yellow">
